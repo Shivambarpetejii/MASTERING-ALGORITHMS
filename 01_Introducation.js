73f17009ff427dc865ae00372b7595cfa342d0c6
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2025-09-03 17:58:57 +0000

    Algorithm introducation

---
+++ 01_Introducation.js
@@ -0,0 +1,8 @@
+// Introducation :- An algorithm is finite sequence of well-defined instructions that takes some input, performs a series of steps, and produces an output to solve a specific problem.
+        //👉 Example:--- Problem: Find the largest number in a list.
+            // Algorithm (Step by Step):-- 
+                                        //1. Assume the first number is the largest.
+                                        //2. Compare it with the next number.
+                                        //3. If the next number is larger, update the "largest".
+                                        //4. Repeat until the list ends.
+                                        //5. Return the largest number.
